d054a2ed640f237969cf35804019b4c65051ea6d
stas <stas@i.ua>
2023-09-07 16:05:55 +0300

    solved exersice

--- app/main.py
+++ app/main.py
@@ -1,3 +1,15 @@
 def cache(func):
     # Write your code here
-    pass
+    cache_dict = {}  # Словник для зберігання результатів
+
+    def wrapper(*args):
+        if args in cache_dict:
+            print("Getting from cache")
+            return cache_dict[args]
+        else:
+            result = func(*args)
+            cache_dict[args] = result
+            print("Calculating new result")
+            return result
+
+    return wrapper
